ddadb992b2349332a418932e6a5e2e36d4e6a5a7
Azran Hossain <mr69751@gmail.com>
2023-01-19 03:32:05 +0600

    done button added

--- src/components/TodosCard.tsx
+++ src/components/TodosCard.tsx
@@ -5,7 +5,12 @@ export default function TodosCard({ desc, date, isDone }: Todo) {
   return (
     <div className="bg-amber-400 p-5 w-[290px] rounded shadow-lg">
       <p className={isDone ? "line-through" : ""}>{desc}</p>
-      <p className="text-right mt-2 border-t pt-2">{date}</p>
+      <div className="flex justify-between mt-2 border-t border-black pt-2">
+        <button className="border border-black px-2 rounded hover:border-white ">
+          Done
+        </button>
+        <p>{date}</p>
+      </div>
     </div>
   );
 }
